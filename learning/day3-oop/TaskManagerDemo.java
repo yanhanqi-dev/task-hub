a7ceb1e293b7c5a1ab5b76ddfb022fe6efaf4044
YanHanqi <133566816+yanhanqi-dev@users.noreply.github.com>
2026-08-25 17:28:14 +0800

    feat: add task list operations

--- learning/day3-oop/TaskManagerDemo.java
+++ learning/day3-oop/TaskManagerDemo.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TaskManagerDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Task> tasks = new ArrayList<>();
+
         while (true) {
             System.out.println("\n=== Task Hub ===");
             System.out.println("1. 添加任务");
@@ -11,15 +15,26 @@ public class TaskManagerDemo {
             System.out.print("请选择功能：");
 
             String choice = scanner.nextLine();
-
-
-
             System.out.println("你选择了" + choice);
 
             if (choice.equals("1")) {
-                System.out.println("添加任务功能准备中");
+                System.out.print("请输入任务名称：");
+                String title = scanner.nextLine();
+
+                int id = tasks.size() + 1;
+                Task NewTask = new Task(id, title, false);
+                tasks.add(NewTask);
+
+                System.out.println("已添加任务：" + NewTask.getTitle());
             } else if (choice.equals("2")) {
-                System.out.println("查看任务功能准备中");
+                if(tasks.isEmpty()) {
+                    System.out.println("当前没有任务。");
+                } else {
+                    System.out.println("=== 任务列表 ===");
+                    for(Task currentTask : tasks) {
+                        System.out.println(currentTask.getId() + ". " + currentTask.getTitle());
+                    }
+                }
             } else if (choice.equals("0")) {
                 System.out.println("已退出 Task Hub，再见！");
                 break;
