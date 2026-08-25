64761255b7fe4a11a3562ea79d6eded6c1ad52e8
YanHanqi <133566816+yanhanqi-dev@users.noreply.github.com>
2026-08-25 14:43:18 +0800

    refactor: encapsulate day3 task object

--- learning/day3-oop/Task.java
+++ learning/day3-oop/Task.java
@@ -1,5 +1,23 @@
-public class Task {
-    int id;
-    String title;
-    boolean completed;
+public class Task{
+    private int id;
+    private String title;
+    private boolean completed;
+
+    public Task(int id, String title, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
 }
--- learning/day3-oop/TaskDemo.java
+++ learning/day3-oop/TaskDemo.java
@@ -1,23 +1,16 @@
 public class TaskDemo {
     public static void main(String[] args) {
 
-        Task task = new Task();
-        task.id = 1;
-        task.title = "学习Java对象";
-        task.completed = true;
+        Task task = new Task(1, "学习 Java 对象", true);
+        Task task2 = new Task(2, "完成 Day 3", false);
 
-        Task task2 = new Task();
-        task2.id = 2;
-        task2.title = "完成 Day 3";
-        task2.completed = false;
-
-        System.out.println("编号： " + task.id);
-        System.out.println("标题： " + task.title);
-        System.out.println("已完成： " + task.completed);
+        System.out.println("编号： " + task.getId());
+        System.out.println("标题： " + task.getTitle());
+        System.out.println("已完成： " + task.isCompleted());
 
         System.out.println("第二条任务：");
-        System.out.println("编号： " + task2.id);
-        System.out.println("标题： " + task2.title);
-        System.out.println("已完成： " + task2.completed);
+        System.out.println("编号： " + task2.getId());
+        System.out.println("标题： " + task2.getTitle());
+        System.out.println("已完成： " + task2.isCompleted());
     }
 }
