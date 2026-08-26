03015074d7a062b3aa921a132591e3280bb3ddfe
YanHanqi <133566816+yanhanqi-dev@users.noreply.github.com>
2026-08-26 11:58:41 +0800

    feat: add task creation API

--- backend/src/main/java/com/yanhanqi/taskhub/controller/TaskController.java
+++ backend/src/main/java/com/yanhanqi/taskhub/controller/TaskController.java
@@ -1,19 +1,33 @@
 package com.yanhanqi.taskhub.controller;
 
 import com.yanhanqi.taskhub.model.Task;
+import com.yanhanqi.taskhub.model.CreateTaskRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TaskController {
+    private final List<Task> tasks =  new ArrayList<>(List.of(
+            new Task(1L, "学习 Day 11", false),
+            new Task(2L, "学习 REST API", true)
+    ));
+
 
     @GetMapping("/api/tasks")
     public List<Task> listTasks() {
-        return List.of(
-                new Task(1L, "完成 Day 11", false),
-                new Task(2L, "学习 REST API", true)
-        );
+        return tasks;
+    }
+
+    @PostMapping("/api/tasks")
+    public Task createTask(@RequestBody CreateTaskRequest request) {
+        long id = tasks.size() + 1L;
+        Task task = new Task(id, request.getTitle(), false);
+        tasks.add(task);
+        return task;
     }
 }
---
+++ backend/src/main/java/com/yanhanqi/taskhub/model/CreateTaskRequest.java
@@ -0,0 +1,14 @@
+package com.yanhanqi.taskhub.model;
+
+
+public class CreateTaskRequest {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
